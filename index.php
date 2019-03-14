a4c3f5cc0f336383353d67528f3ee535f5ff2b4d
Gabriel Almeida <galmeida9@gmail.com>
2019-03-14 22:52:08 +0000

    added form

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include("./simplestats/simplestats.inc"); ?>
+<?php include("./simplestats/simplestats.inc");?>
 
 <!DOCTYPE html>
 <html>
@@ -11,6 +11,7 @@
 <link rel="stylesheet" href="slideshow.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="jquery-eu-cookie-law-popup.css"/>
+<link rel="icon" href="resources/resume.png">
 <style>
 body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 
@@ -36,7 +37,6 @@ body, html {
 
 </style>
 <body class="eupopup eupopup-bottom">
-
 
 <!-- Navbar (sit on top) -->
 <div class="w3-top">
@@ -101,13 +101,13 @@ body, html {
 <!-- Sobre mim -->
 <div class="w3-container" style="padding:128px 16px" id="about">  
   <div class="w3-center">
-    <img src="resources/profile.png" style="border-radius: 50%; width: 200px; padding-bottom: 20px;">
+    <img src="resources/profile.png" style="width: 400px; padding-bottom: 20px;">
     <h3 class="w3-center">SOBRE MIM</h3>
     <p class="w3-center w3-large">Sou estudante de Engenharia Informática e de Computadores no Instituto Superior Técnico. </p>
     <h3 class="w3-center" style="padding-top: 25px">Biografia</h3>
     <p class="w3-center w3-large">Nasci em 1999 e fiz a minha escolaridade obrigatória toda em Coimbra, no Colégio Apostólico da Imaculada Conceição, acabando com média de 18.</p>
     <p class="w3-center w3-large">Desde pequeno que gostava de informática e com o tempo essa paixão foi ficando cada vez maior, tendo influência também da parte do meu pai. 
-    Então, decidi-me aventurar e ir estudar em Lisboa, devido ao prestigio do IST, indo já no segundo ano da Licenciatura. Sempre me fascinei e tive interesse em Inteligência Artificial, sendo por isso que estou a ponderar
+    Então, decidi-me aventurar e ir estudar em Lisboa, devido ao prestigio do IST, indo já no segundo ano da Licenciatura com média atual de 16,40. Sempre me fascinei e tive interesse em Inteligência Artificial, sendo por isso que estou a ponderar
   a tirar mestrado nessa área. Para desenvolver as minhas capacidades, tenho feito alguns projetos nos tempos livres, alguns dos quais disponíveis neste site. Atualmente gostaria imenso de participar
 num estágio de verão para adquirir experiência.</p>
   </div>
@@ -136,7 +136,7 @@ num estágio de verão para adquirir experiência.</p>
         de como fazê-lo.
       </p>
       <p>Em Java tenho em baixo um pequeno projeto pessoal, o jogo 2048, com interface gráfica, feito por mim de raíz.</p>
-      <button id="butaocv" style="margin-top:40px"><a href="resources/gabriel_cv.pdf" download style="text-decoration:none;">MEU CV</a></button>
+      <button id="butaocv" style="margin-top:40px"><a href="resources/gabriel_cv.pdf" download style="text-decoration:none">MEU CV</a></button>
       <p>(ainda por atualizar)</p>
     </div>
     <div class="w3-col m6">
@@ -146,19 +146,19 @@ num estágio de verão para adquirir experiência.</p>
       </div>
       <p class="w3-wide"><i class="fa fa-desktop w3-margin-right"></i>Java</p>
       <div class="w3-grey">
-        <div class="w3-container w3-dark-grey w3-center" style="width:90%">90%</div>
+        <div class="w3-container w3-dark-grey w3-center" style="width:85%">85%</div>
       </div>
       <p class="w3-wide"><i class="fa fa-desktop w3-margin-right"></i>Python</p>
       <div class="w3-grey">
         <div class="w3-container w3-dark-grey w3-center" style="width:85%">85%</div>
       </div>
+      <p class="w3-wide"><i class="fa fa-desktop w3-margin-right"></i>Prolog</p>
+      <div class="w3-grey">
+        <div class="w3-container w3-dark-grey w3-center" style="width:75%">75%</div>
+      </div>
       <p class="w3-wide"><i class="fa fa-desktop w3-margin-right"></i>HTML/CSS/JavaScript</p>
       <div class="w3-grey">
         <div class="w3-container w3-dark-grey w3-center" style="width:50%">50%</div>
-      </div>
-      <p class="w3-wide"><i class="fa fa-desktop w3-margin-right"></i>Prolog</p>
-      <div class="w3-grey">
-        <div class="w3-container w3-dark-grey w3-center" style="width:45%">45%</div>
       </div>
       <p class="w3-wide"><i class="fa fa-desktop w3-margin-right"></i>Assembly</p>
       <div class="w3-grey">
@@ -171,7 +171,7 @@ num estágio de verão para adquirir experiência.</p>
 <!-- O que eu fiz-->
 <div class="w3-container w3-dark-grey w3-center" style="padding:128px 16px" id="work">
   <h3>PORTFOLIO</h3>
-  <p class="w3-large">O que eu fiz até agora</p>
+  <p class="w3-large">What I've done so far</p>
   <div class="w3-row-padding" style="margin-top:64px">
     <div class="w3-col l3 m6">
       <img src="resources/IST_Logo.png" style="width:50%; padding-top: 15px" onclick="onClick(this, 'IST_Logo.png')" class="w3-hover-opacity" alt="Alguns dos projetos que realizei no curso">
@@ -186,7 +186,7 @@ num estágio de verão para adquirir experiência.</p>
 <div id="modal01" class="w3-modal w3-black" onclick="this.style.display='none'">
   <span class="w3-button w3-xxlarge w3-black w3-padding-large w3-display-topright" title="Close Modal Image">×</span>
   <div class="w3-modal-content w3-animate-zoom w3-center w3-transparent w3-padding-64">
-    <img id="img01" class="w3-image">
+    <img id="img01" class="w3-image" style="width: 50%">
     <p id="caption01" class="w3-opacity w3-large"></p>
 	<p><a href="https://github.com/galmeida9/LEIC-A-Projects" target="_blank">Link para o Github</a></p>
   </div>
@@ -195,7 +195,7 @@ num estágio de verão para adquirir experiência.</p>
 <div id="modal02" class="w3-modal w3-black" onclick="this.style.display='none'">
   <span class="w3-button w3-xxlarge w3-black w3-padding-large w3-display-topright" title="Close Modal Image">×</span>
   <div class="w3-modal-content w3-animate-zoom w3-center w3-transparent w3-padding-64">
-    <img id="img02" class="w3-image">
+    <img id="img02" class="w3-image" style="width: 50%">
     <p id="caption02" class="w3-opacity w3-large"></p>
 	<p><a href="https://github.com/galmeida9/The-2048-Game" target="_blank">Link para o Github</a></p>
   </div>
@@ -208,10 +208,37 @@ num estágio de verão para adquirir experiência.</p>
     <p><i class="fa fa-phone fa-fw w3-xxlarge w3-margin-right"></i> Telemóvel: +351 917 308 440</p>
     <p><i class="fa fa-envelope fa-fw w3-xxlarge w3-margin-right"> </i> Email pessoal: <a href="mailto:galmeida9@gmail.com">galmeida9@gmail.com</a></p>
     <p><i class="fa fa-envelope fa-fw w3-xxlarge w3-margin-right"> </i> Email da universidade: <a href="mailto:>gabriel.almeida@tecnico.ulisboa.pt">gabriel.almeida@tecnico.ulisboa.pt</a></p>
-    <p><i class="fa fa-facebook fa-fw w3-xxlarge w3-margin-right"> </i> Redes sociais encontram-se em baixo e no slideshow</p>
     <br>
   </div>
+  <h4 style="padding-left: 10px">Redes Sociais:</h4>
+  <div class="w3-xxlarge w3-margin-right w3-section">
+    <a href="https://www.facebook.com/gabriel.almeida.18062533" target="_blank"><i class="fa fa-facebook-official w3-hover-opacity" style="padding-left: 10px"></i></a>
+    <a href="https://www.instagram.com/galmeida9/" target="_blank"><i class="fa fa-instagram w3-hover-opacity"></i></a>
+    <a href="https://twitter.com/GBAlmeida9" target="_blank"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+    <a href="https://www.linkedin.com/in/gabriel-a-b3772a131/" target="_blank"><i class="fa fa-linkedin w3-hover-opacity"></i></a>
+  </div>
+  
+  <!-- enviar mensagem -->
+  <div class="w3-row-padding">
+    <div class="w3-col m12 w3-center">
+      <h3>Envia-me um email!</h3>
+	    <div class="w3-container">
+	      <form action="php/mail.php" method="post">
+	        <p><input class="w3-input w3-border" type="text" placeholder="Nome" required name="Nome"></p>
+          <p><input class="w3-input w3-border" type="text" placeholder="E-mail" required name="Email"></p>
+          <p><input class="w3-input w3-border" type="text" placeholder="Título" required name="Titulo"></p>
+		      <p><input class="w3-input w3-border" type="text" placeholder="Comentário" required name="Comentario"></p>
+		      <p>
+		        <button class="w3-button w3-black" type="submit">
+			        <i class="fa fa-paper-plane"></i> Enviar
+		        </button>
+		      </p>
+        </form>
+      </div>
+	  </div>
+  </div>
 </div>
+
 
 <!-- Rodapé -->
 <footer class="w3-center w3-black w3-padding-64">
@@ -266,7 +293,7 @@ function onClick(element, img) {
 		captionText.innerHTML = element.alt;
 	}
 	else if (img == "2048_Logo.png") {
-		document.getElementById("img02").src = element.src;
+		document.getElementById("img02").src = "resources/2048_Screenshot.png";
 		document.getElementById("modal02").style.display = "block";
 		var captionText = document.getElementById("caption02");
 		captionText.innerHTML = element.alt;
