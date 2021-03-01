5de598fc251a4ac5a9269ca8a844040df5c201da
Gabriel Almeida <galmeida9@gmail.com>
2021-03-01 23:10:31 +0000

    improved jap design and fix mobile animation

--- js/translate.js
+++ js/translate.js
@@ -1,13 +1,34 @@
-function changeHobbieLang() {
+function changeHobbieLang(lang) {
     let jap = document.getElementById("japaneseBio");
     let eng = document.getElementById("englishBioTranslation");
+    let jpnButton = document.getElementById("jpnBtn");
+    let engButton = document.getElementById("engBtn");
 
-    if (jap.style.display == "block") {
+    if (lang === "eng") {
         jap.style.display = "none";
         eng.style.display = "block";
+        jpnButton.style.color = "white";
+        engButton.style.color = "grey";
     }
     else {
         jap.style.display = "block";
         eng.style.display = "none";
+        jpnButton.style.color = "grey";
+        engButton.style.color = "white";
+    }
+}
+
+addAnimationToTimeline();
+
+function addAnimationToTimeline() {
+    if (window.innerWidth > 768) {
+        document.getElementsByClassName("timeContainer")[0].classList.add("scroll-animated");
+    }
+    else {
+        var lis = document.getElementsByClassName("timelineBubble");
+        console.log(lis)
+        for (let i = 0; i < lis.length; i++) {
+            lis[i].classList.add("scroll-animated");
+        }
     }
 }
